5c543599eba86ce1c2764e58541848f65eba1b0c
KarolinaMonkeviciute <145319640+KarolinaMonkeviciute@users.noreply.github.com>
2024-02-27 14:12:13 +0200

    fix

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,17 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\Item;
 use App\Models\Currency;
-use App\Http\Requests\StoreItemRequest;
-use App\Http\Requests\UpdateItemRequest;
 use App\Services\GetSelectedCurrencyRateService;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class ItemController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index(Request $request): View
     {
         $items = Item::all();
         $currencies = Currency::all();
@@ -32,53 +31,5 @@ class ItemController extends Controller
             'selectedCurrency' => $selectedCurrency,
             'rate' => $rate,
         ]);
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(StoreItemRequest $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Item $item)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Item $item)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(UpdateItemRequest $request, Item $item)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Item $item)
-    {
-        //
     }
 }
--- app/Services/GetSelectedCurrencyRateService.php
+++ app/Services/GetSelectedCurrencyRateService.php
@@ -5,9 +5,14 @@ use App\Models\Currency;
 
 class GetSelectedCurrencyRateService
 {
-    public static function getRate(string $currency): float{
-        if($currency){
-           return Currency::where('currency_code', $currency)->first()->rate;
+    public static function getRate(string $currency): ?float{
+        if($currency){ 
+           $findCurrency = Currency::where('currency_code', $currency)->first();
+           if($findCurrency){
+            return $findCurrency->rate;
+           }else{
+            abort(404);
+           }
         }
     }
 }
